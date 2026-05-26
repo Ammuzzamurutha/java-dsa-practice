9aa82225908924e6872b777263046823b80dfa6e
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-05-26 10:54:18 +0530

    Add files via upload

---
+++ shoppingcart.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class shoppingcart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String item;
+        double price;
+        int quantity;
+        char currency='$';
+        double total;
+
+        System.out.print("What item would you like to purchase? ");
+        item = sc.nextLine();
+        System.out.print("What is the price of the item? ");
+        price = sc.nextDouble();
+        System.out.print("How many would you like to purchase? ");
+        quantity = sc.nextInt();
+        total = price * quantity;
+        System.out.println("You have purchased " + quantity + " " + item + "(s) for a total of " + currency + total);
+    }
+}
